42987123e4cd52851fd386aae5fa55803fb0bdbb
Ida <iwang2@stuy.edu>
2017-05-31 15:29:53 -0400

    basic skeleton sort of

---
+++ Molecule.java
@@ -0,0 +1,22 @@
+public class Molecule{
+    private ArrayList<Atom> atoms;
+    private double x, y, z;
+    private double vx, vy, vz;
+    private double acceleration;
+
+    // default molecule is a single hydrogen atom
+    public Molecule(){
+	atoms.add(new Atom());
+    }
+
+    /* force stuff--idk which methods are needed exactly
+    private double getCovalentForce(){
+    }
+    private double getKineticEnergy(){
+    }
+    */
+
+    public void updateAcceleration(){
+	//private methods called here
+    }
+}
